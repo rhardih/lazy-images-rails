485447ac9064a5f0637ee0b30d63d343f60ce0f9
René Hansen <renehh@gmail.com>
2015-08-19 21:37:53 +0200

    Moves html snippet into fixture file

--- test/lazy_images_test.rb
+++ test/lazy_images_test.rb
@@ -5,13 +5,9 @@ class LazyImagesTest < ActionView::TestCase
     assert_kind_of Module, LazyImages
   end
 
-    test "image_tag outputs wrapped image with placeholder and noscript" do
-      markup = %(<div data-rli-image-src="/images/foo.png" data-rli-placeholder="true" class="rli-wrapper"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" class="rli-placeholder">
-  <path class="moon" d="M832 288c0 53.020-42.98 96-96 96s-96-42.98-96-96 42.98-96 96-96 96 42.98 96 96z"></path>
-  <path class="mountain" d="M896 832h-768v-128l224-384 256 320h64l224-192z"></path>
-</svg>
-<noscript><img src="/images/foo.png" class=" rli-image" alt="Foo" /></noscript></div>)
+  test "image_tag outputs wrapped image with placeholder and noscript" do
+    expected = File.read(File.expand_path("../fixtures/_image_tag_output.html", __FILE__))
 
-    assert_dom_equal markup, image_tag('foo.png')
+    assert_dom_equal(expected, image_tag('foo.png'))
   end
 end
